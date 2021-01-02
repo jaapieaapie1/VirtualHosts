c8e94f9a545b7149966a279d5256425bc1a40f6f
jaapie_aapie <63400965+jaapieaapie1@users.noreply.github.com>
2021-01-02 23:27:44 +0100

    added things for javadoc

--- src/main/java/nl/jaapie/virtualhosts/VirtualHost.java
+++ src/main/java/nl/jaapie/virtualhosts/VirtualHost.java
@@ -1,15 +1,31 @@
 package nl.jaapie.virtualhosts;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import net.md_5.bungee.config.Configuration;
 
 @Getter
-@AllArgsConstructor()
 public class VirtualHost {
 
+    /**
+     * Host name
+     */
     private final String host;
+    /**
+     * Server name
+     */
     private final String server;
+
+    /**
+     * Virtual host
+     * @param host hostname
+     * @param server sername
+     */
+    public VirtualHost(String host, String server) {
+        this.host = host;
+        this.server = server;
+    }
 
     /**
      * Create from config section
